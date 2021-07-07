27b374efdb4f2451df60ee745538a39cb6bb8f7b
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-07 18:33:34 +0530

    Checkout

---
+++ src/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Checkout.css";
+
+
+function Checkout() {
+    return (
+        <div>
+            <h1>
+                Smash
+            </h1>
+        </div>
+    );
+}
+
+export default Checkout
