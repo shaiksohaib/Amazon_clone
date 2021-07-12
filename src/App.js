cab41d8e7eaac5d4251f5bdecb2479664113c53b
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-12 23:54:52 +0530

    adding app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
- import React from 'react';
+ import React ,{ useEffect } from 'react';
 
 import './App.css';
 import Header from './Header';
 import Home from "./Home";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
+import {auth } from "./firebase";
+import {useStateValue} from "./StateProvider";
 
+function App () {
 
-function App() {
+  const [{},dispatch] = useStateValue();
+  useEffect (() => {
+    //only run once app component loads
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>',authUser);
+
+      if (authUser) {
+        //the user just logged in / the user was logged in
+        dispatch({
+          type:'SET_USER',
+          user: authUser
+        })
+      }else{
+        //the user is logged out
+        dispatch({
+          type:'SET_USER',
+          user: null
+        })
+      
+      }
+    })
+  }, [])
   return (
     //BEM
-    <Router>
-    <div className="app">
-   
-      <Switch>
-        <Route Path="/">
-        <Header/>
-         <Home />
+    <BrowserRouter>
+      <div className="app">
+    <Switch>
+    
+        <Route path="/login" exact>
+          <Login/>
         </Route>
-        <Route Path="/Checkout">
+        <Route path= "/checkout" exact>
         <Header/>
-         <Checkout/> 
+        <Checkout/>
         </Route>
-        
-      </Switch>
+        <Route path="/">
+          <Header/>
+          <Home/>
+        </Route>
+     </Switch>
    </div>
-   </Router>
+   </BrowserRouter>
   );
 }
+
 
 export default App;
