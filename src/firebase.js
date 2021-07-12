4bf28fe653bbc1a8d67983cb033e1337f74f4e17
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-12 23:55:55 +0530

    adding firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBezQDoBytfaGzoLN2MYJWQpp51h35_8qU",
@@ -8,3 +10,8 @@ const firebaseConfig = {
     appId: "1:142122922740:web:43155bae4e7af6f41864a1",
     measurementId: "G-4BNKBT28HE"
   };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db,auth};
