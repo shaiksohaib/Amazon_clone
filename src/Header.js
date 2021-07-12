a3446dbcd41ca91ac3a74f5ea0825157ef65927f
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-12 23:55:22 +0530

    adding Header

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,25 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-
+import {Link} from "react-router-dom";
+import {useStateValue} from "./StateProvider"
+import { auth} from "./firebase";
 function Header() {
+    const [{basket, user},dispatch] = useStateValue();
+
+    const handleAuthentication = () => {
+        if(user){
+          auth.SignOut();
+        }
+    }
     return (
         <div  className='header'>
-            
-            <img className="header_logo"
+            <Link to="/"
+            ><img className="header_logo"
               src="https://th.bing.com/th/id/OIP.Au9CFZ2If7pjAArnBggZ7QHaEn?w=253&h=180&c=7&o=5&dpr=1.25&pid=1.7 "/>
+              </Link>
+            
+            
             <div className="header_search">
                 <input className="header_searchInput"
                  type="text"/>
@@ -18,40 +30,46 @@ function Header() {
                 </div>
                 
             
+
         <div className="header_nav">
-            <div className='header_option'>
+            <Link to={!user && '/Login'}>
+        
+            <div  onClick ={handleAuthentication} className='header_option'>
                 <span className='header_optionLineOne'>Hello Guest</span>
-                <span className='header_optionLineTwo'>Sign In</span>
-                </div>
+                <span className='header_optionLineTwo'>{user ? 'Sign Out': 'Sign In'}</span>
+            </div>
+            </Link>
             <div className='header_option'>
-            <span className='header_optionLineOne'>Returns</span>
+                <span className='header_optionLineOne'>Returns</span>
                 <span className='header_optionLineTwo'>Orders</span>
-
-
             </div>
 
             <div className='header_option'>
               <span className='header_optionLineOne'>Your</span>
                 <span className='header_optionLineTwo'>Prime</span>
-
-
             </div>
-            </div>
+            
+            <Link to="/checkout">
             <div className="header_optionBasket">
                 <ShoppingBasketIcon/>
                 <span className="header_optionLineTwo  
-                header_basketCount">0</span>
+                header_basketCount">
+                    {basket?.length}
+                </span>
+            </div>
+                </Link>
+             </div>
                  
             </div>
                 
             
-            </div>
+            
             
 
 
         
         
-    )
+    );
 }
 
 export default Header;
